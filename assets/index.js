940c49b1751eb8dc63cdc05325dce3bbbf892612
Gabriel Allba <rielgasy9@gmail.com>
2022-01-28 08:27:07 +0700

    add attributes and about product name

--- assets/index.js
+++ assets/index.js
@@ -31,10 +31,15 @@ outside.addEventListener('click', () => {
 let aboutOpenner = document.getElementById('about-openner-acc')
 let detailOpenner= document.getElementById('detail-openner-acc')
 let offersOpenner= document.getElementById('offers-openner-acc')
+let attributesOpenner =document.getElementById('attribute-openners')
+let aboutProductOpenner = document.getElementById('about-product-openners')
 
 let accordionDetailAbout = document.getElementById('acc-detail-about')
 let accordionDetailDetail = document.getElementById('acc-detail-detail')
 let accordionDetailOffers= document.getElementById('acc-detail-offers')
+let accordionDetailAttributes = document.getElementById('acc-detail-attributes')
+let accordionDetailProduct= document.getElementById('acc-detail-product')
+
 
 
 
@@ -47,4 +52,10 @@ detailOpenner.addEventListener('click', () => {
 })
 offersOpenner.addEventListener('click', () => {
     accordionDetailOffers.classList.toggle('hide-accordion')
+})
+attributesOpenner.addEventListener('click', () =>{
+    accordionDetailAttributes.classList.toggle('hide-accordion')
+})
+aboutProductOpenner.addEventListener('click', () => {
+    accordionDetailProduct.classList.toggle('hide-accordion')
 })
